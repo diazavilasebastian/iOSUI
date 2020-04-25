8648191d5434bfc6cdfd2a6775541f73d15decc9
Sebastian Diaz Avila <diazavilasebastian@gmail.com>
2020-04-24 21:14:22 -0400

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-iOSUI_Example/Pods-iOSUI_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSUI_Example : NSObject
+@end
+@implementation PodsDummy_Pods_iOSUI_Example
+@end
---
+++ Example/Pods/Target Support Files/iOSUI/iOSUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSUI : NSObject
+@end
+@implementation PodsDummy_iOSUI
+@end
