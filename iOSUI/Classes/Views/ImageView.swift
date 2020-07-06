ab047bcb6a4a0c829bfd97da0d6df74713be8a6a
Sebastian Diaz Avila <diazavilasebastian@gmail.com>
2020-07-06 11:17:00 -0400

    Fix bad identation

--- iOSUI/Classes/Views/ImageView.swift
+++ iOSUI/Classes/Views/ImageView.swift
@@ -32,17 +32,17 @@ public class ImageView: UIView {
     }
 
     func setupConstrains() {
-           self.addSubview(poster)
+        self.addSubview(poster)
 
-           NSLayoutConstraint.activate([
-               .init(item: poster, attribute: .topMargin, relatedBy: .equal, toItem: self, attribute: .topMargin, multiplier: 1.0, constant: 0),
-               .init(item: poster, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0),
-               .init(item: self, attribute: .bottom, relatedBy: .equal, toItem: poster, attribute: .bottom, multiplier: 1.0, constant: 0),
-               .init(item: self, attribute: .trailing, relatedBy: .equal, toItem: poster, attribute: .trailing, multiplier: 1.0, constant: 0)
-           ])
+        NSLayoutConstraint.activate([
+            .init(item: poster, attribute: .topMargin, relatedBy: .equal, toItem: self, attribute: .topMargin, multiplier: 1.0, constant: 0),
+            .init(item: poster, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0),
+            .init(item: self, attribute: .bottom, relatedBy: .equal, toItem: poster, attribute: .bottom, multiplier: 1.0, constant: 0),
+            .init(item: self, attribute: .trailing, relatedBy: .equal, toItem: poster, attribute: .trailing, multiplier: 1.0, constant: 0)
+        ])
 
-           self.backgroundColor = .white
-           self.layer.cornerRadius = 5.0
-           self.clipsToBounds = true
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 5.0
+        self.clipsToBounds = true
     }
 }
