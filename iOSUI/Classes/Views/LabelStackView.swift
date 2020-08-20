aa83237215fb961958646904bf710061526f4b2b
sebastian <sebastiaan.diaz.avila@accenture.com>
2020-08-20 15:50:02 -0400

    Add lint for project

--- iOSUI/Classes/Views/LabelStackView.swift
+++ iOSUI/Classes/Views/LabelStackView.swift
@@ -9,13 +9,13 @@ import Foundation
 import UIKit
 
 public class LabelStackView: UIView {
-    
+
     var title: String
     var axis: NSLayoutConstraint.Axis
     var distribution: UIStackView.Distribution
     var spacing: CGFloat = 5.0
     var isScrollEnabled: Bool = true
-    
+
     lazy var titleLabel: UILabel = {
         let label = UILabel(frame: .zero)
         label.text = self.title
@@ -24,20 +24,20 @@ public class LabelStackView: UIView {
         label.textColor = .white
         return label
     }()
-    
+
     lazy var scrollView: UIScrollView = {
         let scroll = UIScrollView()
         scroll.isScrollEnabled = self.isScrollEnabled
         scroll.translatesAutoresizingMaskIntoConstraints = false
         return scroll
     }()
-    
+
     lazy var contentView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     public lazy var stackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
         stackView.axis = self.axis
@@ -46,12 +46,12 @@ public class LabelStackView: UIView {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-    
+
     public init(title: String,
-         axis: NSLayoutConstraint.Axis,
-         distribution: UIStackView.Distribution,
-         spacing: CGFloat,
-         frame: CGRect = .zero) {
+                axis: NSLayoutConstraint.Axis,
+                distribution: UIStackView.Distribution,
+                spacing: CGFloat,
+                frame: CGRect = .zero) {
         self.title = title
         self.axis = axis
         self.spacing = spacing
@@ -59,36 +59,36 @@ public class LabelStackView: UIView {
         self.isScrollEnabled = axis == .horizontal
         super.init(frame: frame)
         self.setupView()
-        
+
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
 }
 
 extension LabelStackView {
-    
+
     func setupView() {
         configureView()
         addHierarchy()
         addConstraints()
     }
-    
+
     func configureView() {
         self.backgroundColor = .black
     }
-    
+
     func addHierarchy() {
         self.scrollView.addSubview(stackView)
         self.addSubview(titleLabel)
         self.addSubview(scrollView)
     }
-    
+
     func addConstraints() {
-       
+
         NSLayoutConstraint.activate([
            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
@@ -96,20 +96,20 @@ extension LabelStackView {
            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
            stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
         ])
-        
+
         NSLayoutConstraint.activate([
            .init(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0),
            .init(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0),
            .init(item: self, attribute: .trailing, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1.0, constant: 0)
         ])
-        
+
         NSLayoutConstraint.activate([
             .init(item: scrollView, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1.0, constant: 10.0),
             .init(item: scrollView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 10.0),
             .init(item: self, attribute: .trailing, relatedBy: .equal, toItem: scrollView, attribute: .trailing, multiplier: 1.0, constant: 0),
             .init(item: self, attribute: .bottom, relatedBy: .equal, toItem: scrollView, attribute: .bottom, multiplier: 1.0, constant: 0)
         ])
-               
+
     }
-    
+
 }
